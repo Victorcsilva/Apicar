a182043f94907f6cfd414d28085eb6f0dc0c8657
Victorcsilva <95497623+Victorcsilva@users.noreply.github.com>
2022-05-13 20:20:27 -0300

    composição atualização

--- apicar/helpers/utils.js
+++
@@ -1,5 +0,0 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const { PrismaClient } = require("@prisma/client");
-
-export const prisma = new PrismaClient();
